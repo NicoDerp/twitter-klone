1b57f5ae1bc83e9135a2706fd570f2044a3eb936
NicoDerp <derp.nico@gmail.com>
2023-05-07 13:19:44 +0200

    Word count in viewtweet

---
+++ static/javascript/wordCount.js
@@ -0,0 +1,28 @@
+
+function updateWordCount(textarea) {
+    let characterCount = document.querySelector("#characterIndicator");
+    let count = textarea.value.split(' ').length;
+    characterCount.innerText = count + "/250";
+    if (count > 250) {
+        characterCount.classList.add("invalidCharacterIndicator");
+    } else {
+        characterCount.classList.remove("invalidCharacterIndicator");
+    }
+}
+
+function submitBark() {
+    let textarea = document.querySelector("#tweetTextArea");
+    let count = textarea.value.split(' ').length;
+
+    if (count > 250) {
+        let errorMessage = document.querySelector("#errorMessage");
+        errorMessage.innerHTML = "Bark exceeds word limit of 250 words!";
+        errorMessage.style.display = "block";
+        setTimeout(function() {
+            errorMessage.style.display = "none";
+        }, 6000);
+        return false;
+    }
+
+    return true;
+}
